ba740cc2e3c8b38fb4f3f853b6587fc2788049fd
yeoneo kim <yeoneo@yeoneoui-MacBook-Pro.local>
2017-02-14 17:13:36 +0900

    Team Test

--- main.java
+++ main.java
@@ -3,5 +3,7 @@ public class main {
 		System.out.println("Hello world");
 		System.out.println("Hi, professor!!");
 		System.out.println("My name is Tony Chi");
+		System.out.println("My name is Yeoneo Kim");
+
 	}
 }
